5a898f28b8f05b0966605b35a60c9bfb5d98ef2e
SIlvio Lopes <88592543+silvionavas@users.noreply.github.com>
2024-10-22 12:09:48 -0300

    Update ArvoreBinaria.cpp
    Exibir elementos Arvore desenvolvido

--- ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria.cpp
@@ -71,7 +71,7 @@ void menu()
 void inicializar()
 {
 
-	// provisÛrio porque n„o libera a memoria usada pela arvore
+	// provis√≥rio porque n√£o libera a memoria usada pela arvore
 	NO* raiz = NULL;
 	
 	cout << "Arvore inicializada \n";
@@ -152,5 +152,13 @@ int elementosArvore(NO* no)
 
 void exibirElementosArvore(NO* no)
 {
-	
+	if (no == NULL) {
+		return;
+	}
+
+	exibirElementosArvore(no->esq);
+	cout << no->valor << " ";
+	exibirElementosArvore(no->dir
+	);
+
 }
